b4ccf0872c5e323dbd7aec5399c970350bb72d92
raf-coffee <pavel.hexlet@mail.ru>
2025-01-27 23:29:08 +0300

    add lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "no-console": "error",
+    },
+  }),
 ];
 
 export default eslintConfig;
