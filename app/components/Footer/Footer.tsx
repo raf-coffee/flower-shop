4b220b1030bd8cc20bab9f37af42fa46b27119bf
Slava Haikin <vyachowski@gmail.com>
2025-04-20 14:31:13 +0700

    feat: add generated social list

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -7,9 +7,28 @@ import {
   IconBrandTelegram,
 } from "@tabler/icons-react";
 
+const FOOTER_SOCIAL_LIST = [
+  {
+    href: "#",
+    Icon: IconBrandWhatsapp,
+  },
+  {
+    href: "#",
+    Icon: IconBrandTelegram,
+  },
+  {
+    href: "#",
+    Icon: IconBrandFacebook,
+  },
+  {
+    href: "#",
+    Icon: IconBrandInstagram,
+  },
+];
+
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center border-t-2 border-t-main-pink-600 bg-main-pink-400 p-4 font-medium md:p-10 md:pb-4 lg:p-14 lg:pb-4">
+    <footer className="flex flex-col items-center justify-center border-t-2 border-t-main-pink-600 bg-main-pink-400 p-4 font-medium md:p-10 md:pb-4 lg:p-14 lg:pb-12">
       <h2 className="mb-2 md:mb-4 md:text-2xl lg:mb-6 lg:pt-1 lg:text-4xl">
         Контакты:
       </h2>
@@ -27,12 +46,15 @@ export default function Footer() {
         <IconMailFilled className="me-1 text-icons-pink" />
         flowermail@gmail.com
       </a>
-      <div className="mb-2 flex gap-5 text-icons-pink md:mb-4 lg:mb-6">
-        <IconBrandWhatsapp size={32} className="h-auto w-8 md:w-10 lg:w-12" />
-        <IconBrandTelegram size={32} className="h-auto w-8 md:w-10 lg:w-12" />
-        <IconBrandFacebook size={32} className="h-auto w-8 md:w-10 lg:w-12" />
-        <IconBrandInstagram size={32} className="h-auto w-8 md:w-10 lg:w-12" />
-      </div>
+      <ul className="mb-2 flex gap-5 text-icons-pink md:mb-4 lg:mb-8">
+        {FOOTER_SOCIAL_LIST.map(({ Icon, href }, index) => (
+          <li key={`${href}-${index}`}>
+            <a href={href}>
+              <Icon size={32} className="h-auto w-8 md:w-10 lg:w-12" />
+            </a>
+          </li>
+        ))}
+      </ul>
       <p>&copy; {new Date().getFullYear()}. Все права защищены</p>
     </footer>
   );
