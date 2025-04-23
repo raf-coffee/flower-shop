515a0e342b2e69db4b775bd77b2dfc0b5d7bf7fc
Pavel Alloyarov <pavel.hexlet@mail.ru>
2025-04-23 18:31:46 +0300

    [FE-024] refactor the form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./components/Hero/Hero";
 import Sales from "./components/Sales/Sales";
 import WhyUs from "./components/WhyUs/WhyUs";
-import Form from "./components/Form/Form";
+import Form from "./components/OrderForm/OrderForm";
 import Delivery from "./components/Delivery/Delivery";
 
 export default function Home() {
