e5d4998631fb55c82846e888d12355d0647254b2
Slava Haikin <vyachowski@gmail.com>
2025-04-26 20:06:00 +0700

    refactor: add cva style component

--- app/components/ui/Text/Text.tsx
+++ app/components/ui/Text/Text.tsx
@@ -1,23 +1,50 @@
+import { cva } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
 export enum TextSize {
-  EXTRA_SMALL = "text-xs md:text-sm lg:text-base",
-  SMALL = "text-sm md:text-base lg:text-lg",
-  NORMAL = "text-base md:text-lg lg:text-xl",
-  LARGE = "text-lg md:text-xl lg:text-3xl",
-  EXTRA_LARGE = "text-xl md:text-2xl lg:text-4xl",
+  EXTRA_SMALL = "EXTRA_SMALL",
+  SMALL = "SMALL",
+  NORMAL = "NORMAL",
+  LARGE = "LARGE",
+  EXTRA_LARGE = "EXTRA_LARGE",
 }
 
 export enum TextWeight {
-  LIGHT = "font-light",
-  NORMAL = "font-normal",
-  BOLD = "font-bold",
+  LIGHT = "LIGHT",
+  NORMAL = "NORMAL",
+  BOLD = "BOLD",
 }
 
 export enum TextFont {
-  MONTSERRAT = "font-montserrat",
-  LATO = "font-lato",
+  MONTSERRAT = "MONTSERRAT",
+  LATO = "LATO",
 }
+
+const textStyles = cva("", {
+  variants: {
+    size: {
+      [TextSize.EXTRA_SMALL]: "text-xs md:text-sm lg:text-base",
+      [TextSize.SMALL]: "text-sm md:text-base lg:text-lg",
+      [TextSize.NORMAL]: "text-base md:text-lg lg:text-xl",
+      [TextSize.LARGE]: "text-lg md:text-xl lg:text-3xl",
+      [TextSize.EXTRA_LARGE]: "text-xl md:text-2xl lg:text-4xl",
+    },
+    weight: {
+      [TextWeight.LIGHT]: "font-light",
+      [TextWeight.NORMAL]: "font-normal",
+      [TextWeight.BOLD]: "font-bold",
+    },
+    font: {
+      [TextFont.MONTSERRAT]: "font-montserrat",
+      [TextFont.LATO]: "font-lato",
+    },
+  },
+  defaultVariants: {
+    size: TextSize.NORMAL,
+    weight: TextWeight.NORMAL,
+    font: TextFont.MONTSERRAT,
+  },
+});
 
 type TextProps = {
   size?: TextSize;
@@ -36,7 +63,10 @@ export default function Text({
   ...props
 }: TextProps) {
   return (
-    <p className={twMerge(size, weight, font, className)} {...props}>
+    <p
+      className={twMerge(textStyles({ size, weight, font }), className)}
+      {...props}
+    >
       {children}
     </p>
   );
