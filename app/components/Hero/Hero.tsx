ad3dd0ca28dc0af9225a844f0c344f4fc3f106eb
Slava Haikin <vyachowski@gmail.com>
2025-05-03 16:24:58 +0700

    feat: add unified hero component

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -2,16 +2,21 @@ import Image from "next/image";
 import hero2 from "../../../static/hero-lg.png";
 import { Text, TextSize } from "../ui";
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
+import { twMerge } from "tailwind-merge";
 
 export default function Hero({
   heading,
   description,
+  hasBreadCrumbs = false,
+  className,
 }: {
   heading: string;
   description?: string;
+  hasBreadCrumbs?: boolean;
+  className?: string;
 }) {
   return (
-    <div className="bg-main-pink-300">
+    <div className={twMerge("bg-main-pink-300", className)}>
       <div className="relative mx-auto h-[255px] max-w-7xl p-4 md:h-[400px] lg:h-[600px]">
         <Image src={hero2} alt="" fill />
         <h1 className="absolute bottom-20 w-[272px] text-2xl font-bold md:w-[400px] md:text-3xl lg:bottom-44 lg:w-[600px] lg:text-5xl">
@@ -25,7 +30,9 @@ export default function Hero({
             {description}
           </Text>
         )}
-        <Breadcrumbs className="absolute bottom-10 w-[272px] text-2xl font-bold md:w-[400px] md:text-3xl lg:bottom-44 lg:w-[600px] lg:text-5xl" />
+        {hasBreadCrumbs && (
+          <Breadcrumbs className="absolute bottom-10 w-[272px] text-2xl font-bold md:w-[400px] md:text-3xl lg:bottom-44 lg:w-[600px] lg:text-5xl" />
+        )}
       </div>
     </div>
   );
