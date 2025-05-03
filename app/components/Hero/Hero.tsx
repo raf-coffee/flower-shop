fa2cce09a12eb8f4bf8121a616ef4e02b75c293a
Slava Haikin <vyachowski@gmail.com>
2025-05-03 19:01:49 +0700

    feat: add hero components order

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -19,19 +19,22 @@ export default function Hero({
 }) {
   return (
     <section
-      className={twMerge("z-2 relative w-full bg-cover bg-center", className)}
+      className={twMerge(
+        "z-2 relative w-full bg-cover bg-center bg-no-repeat",
+        className,
+      )}
       style={{ backgroundImage: `url(${hero2.src})` }}
     >
       <Container className="flex min-h-[300px] lg:min-h-[700px]">
         <div className="flex flex-col justify-center">
           <div className="flex flex-col justify-center gap-4">
-            <h1 className="w-[272px] text-2xl font-bold md:w-[400px] md:text-3xl lg:bottom-44 lg:w-[600px] lg:text-5xl">
+            <h1 className="lg:order-0 w-[272px] text-2xl font-bold md:w-[400px] md:text-3xl lg:bottom-44 lg:w-[600px] lg:text-5xl">
               {heading}
             </h1>
             {description && (
               <Text
                 size={TextSize.NORMAL}
-                className="md:bottom-40 lg:bottom-72"
+                className="md:bottom-40 lg:bottom-72 lg:-order-1"
               >
                 {description}
               </Text>
