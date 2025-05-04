f0f5369f6205168132da10e2ae436cb815059498
Slava Haikin <vyachowski@gmail.com>
2025-05-04 16:34:00 +0700

    feat: add  contacts text

--- app/(frontend)/contacts/page.tsx
+++ app/(frontend)/contacts/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/app/components/Hero/Hero";
+
 import SocialList from "@/app/components/SocialList/SocialList";
 import {
   Button,
@@ -26,36 +27,54 @@ export default function Contacts() {
               >
                 Звоните нам или пишите в месенджер
               </Text>
-              <div className="flex-col"></div>
-              <a
-                href="tel:+7495111111"
-                className="mb-2 flex items-center md:text-lg lg:mb-1.5 lg:text-2xl"
-              >
-                <IconPhoneCall className="me-1 text-icons-pink" />
-                +7 495 000-00-00
-              </a>
-              <a
-                href="tel:+7495111111"
-                className="mb-2 flex items-center pl-7 md:text-lg lg:mb-1.5 lg:text-2xl"
-              >
-                +7 495 000-00-00
-              </a>
-              <a
-                href="tel:+7495111111"
-                className="mb-2 flex items-center md:text-lg lg:mb-7 lg:text-2xl"
-              >
-                <IconMailFilled className="me-1 text-icons-pink" />
-                flowermail@gmail.com
-              </a>
-              <div className="mb-4 flex items-center justify-between">
+              <div className="flex-col">
+                <a
+                  href="tel:+7495111111"
+                  className="mb-2 flex items-center md:text-lg lg:mb-1.5 lg:text-2xl"
+                >
+                  <IconPhoneCall className="me-1 text-icons-pink" />
+                  +7 495 000-00-00
+                </a>
+                <a
+                  href="tel:+7495111111"
+                  className="mb-2 flex items-center pl-7 md:text-lg lg:mb-1.5 lg:text-2xl"
+                >
+                  +7 495 000-00-00
+                </a>
+                <a
+                  href="tel:+7495111111"
+                  className="mb-2 flex items-center md:text-lg lg:mb-7 lg:text-2xl"
+                >
+                  <IconMailFilled className="me-1 text-icons-pink" />
+                  flowermail@gmail.com
+                </a>
+              </div>
+              <div className="flex items-center justify-between">
                 <Text size={TextSize.NORMAL} font={TextFont.LATO}>
                   Мы в соц сетях:
                 </Text>
                 <SocialList />
               </div>
             </div>
-            <div className=""></div>
-            <div className=""></div>
+            <div>
+              <Text
+                className="mb-4"
+                size={TextSize.LARGE}
+                weight={TextWeight.BOLD}
+              >
+                Как нас найти?
+              </Text>
+              <Text className="mb-2" size={TextSize.NORMAL}>
+                Город, улица Название, дом 1
+              </Text>
+              <Text size={TextSize.NORMAL}>
+                Режим работы: Пн-пт: 00:00-00:00
+              </Text>
+              <Text size={TextSize.NORMAL}>Сб-вс: 00:00-00:00</Text>
+            </div>
+            <div className="min-h-[350px] w-full min-w-[350px]">
+              {/* HERE GOES A MAP */}
+            </div>
           </div>
         </Container>
       </section>
