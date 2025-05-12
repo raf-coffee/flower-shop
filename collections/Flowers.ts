b0d728771f20755fdfbfa08ad5cedf7f58440e82
Pavel Alloyarov <pavel.hexlet@mail.ru>
2025-05-12 18:06:38 +0300

    refactor collections

--- collections/Flowers.ts
+++ collections/Flowers.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from "payload";
 
 export const Flowers: CollectionConfig = {
   slug: "flowers",
+  access: {
+    read: () => true,
+  },
   fields: [
     {
       name: "name",
--- collections/Tags.ts
+++ collections/Tags.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from "payload";
 
 export const Tags: CollectionConfig = {
   slug: "tags",
+  access: {
+    read: () => true,
+  },
   fields: [
     {
       name: "name",
