6279df3717a6046238eb785729d5ee7bb2d37182
Pavel Alloyarov <pavel.hexlet@mail.ru>
2025-05-23 06:43:29 +0300

    add Card and Carousel components

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,30 +1,35 @@
 import Image from "next/image";
+import { ProductCollections } from "@/types";
+import { Config } from "@/payload-types";
 
-type Item = {
-  item: {
-    id: string;
-    title: string;
-    price: number;
-    image: {
-      src: string;
-      alt: string;
-    };
-  };
-};
-
-export default function Card({ item }: Item) {
+export default function Card<T extends ProductCollections>({
+  item,
+}: {
+  item: Config["collections"][T];
+}) {
   return (
     <div className="flex h-full flex-col justify-between rounded-lg bg-white p-1">
       <div className="relative h-[145px] min-[900px]:h-[245px]">
-        <Image
-          src={item.image.src}
-          alt={item.image.alt}
-          fill
-          className="rounded-t-lg"
-        />
+        {item.images &&
+          item.images.map((image, index) => {
+            if (typeof image === "number") {
+              return <p key={index}>картинка</p>;
+            }
+            return (
+              image.url && (
+                <Image
+                  src={image.url}
+                  alt=""
+                  fill
+                  className="rounded-t-lg"
+                  key={image.id}
+                />
+              )
+            );
+          })}
       </div>
       <div className="mx-auto max-w-[170px] min-[900px]:max-w-[225px] lg:max-w-[265px]">
-        <h3 className="text-xs md:text-sm">{item.title}</h3>
+        <h3 className="text-xs md:text-sm">{item.name}</h3>
       </div>
       <div className="my-2 flex items-center justify-evenly">
         <p className="text-green-800">{item.price}&#8381;</p>
