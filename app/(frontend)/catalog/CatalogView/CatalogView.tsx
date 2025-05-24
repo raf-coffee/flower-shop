8e004a2c806a836728dfdba8b8b2acd80ee8570d
Slava Haikin <vyachowski@gmail.com>
2025-05-24 22:58:10 +0300

    feat: add image covers, checkboxes

--- app/(frontend)/catalog/CatalogView/CatalogView.tsx
+++ app/(frontend)/catalog/CatalogView/CatalogView.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import {
   Button,
   Heading,
-  RadioButton,
+  Checkbox,
   Text,
   Range,
   TextWeight,
@@ -13,7 +13,7 @@ import {
 } from "@/app/components/ui";
 import Link from "next/link";
 import Image from "next/image";
-import { Category, Occasion, Whom } from "@/payload-types";
+import { Category, Media, Occasion, Whom } from "@/payload-types";
 import { DataFromCollectionSlug } from "payload";
 
 function CatalogView({
@@ -42,13 +42,11 @@ function CatalogView({
     max: maxPrice,
   });
 
-  const [activeCategory, setActiveCategory] = useState(
-    filters.categories?.[0]?.id,
-  );
-  const [activeOccasion, setActiveOccasion] = useState(
-    filters.occasions?.[0]?.id,
-  );
-  const [recipientType, setRecipientType] = useState(filters.whoms?.[0]?.id);
+  // const [activeCategory, setActiveCategory] = useState(
+  const [activeCategory] = useState(filters.categories?.[0]?.id);
+  // const [activeOccasion, setActiveOccasion] = useState(
+  const [activeOccasion] = useState(filters.occasions?.[0]?.id);
+  const [recipientType] = useState(filters.whoms?.[0]?.id);
 
   useEffect(() => {
     setFilteredProducts(products);
@@ -62,39 +60,48 @@ function CatalogView({
             <legend>
               <Heading level={3}>Категории</Heading>
             </legend>
-            <RadioButton
-              name="category_1"
-              checked={false}
-              onClick={() => setActiveCategory(1)}
-            >
-              Вариант 1
-            </RadioButton>
+            {filters.categories.map((cat) => (
+              <Checkbox
+                name="category"
+                checked={false}
+                onClick={() => null}
+                key={cat.id}
+              >
+                {cat.name}
+              </Checkbox>
+            ))}
           </fieldset>
 
           <fieldset className="mb-2">
             <legend>
               <Heading level={3}>Повод</Heading>
             </legend>
-            <RadioButton
-              name="category_2"
-              checked={activeOccasion === 1}
-              onClick={() => setActiveOccasion(1)}
-            >
-              Вариант 1
-            </RadioButton>
+            {filters.occasions.map((cat) => (
+              <Checkbox
+                name="occasion"
+                checked={false}
+                onClick={() => null}
+                key={cat.id}
+              >
+                {cat.name}
+              </Checkbox>
+            ))}
           </fieldset>
 
           <fieldset>
             <legend>
               <Heading level={3}>Кому</Heading>
             </legend>
-            <RadioButton
-              name="category_3"
-              checked={recipientType === 1}
-              onClick={() => setRecipientType(1)}
-            >
-              Вариант 1
-            </RadioButton>
+            {filters.whoms.map((cat) => (
+              <Checkbox
+                name="whom"
+                checked={false}
+                onClick={() => null}
+                key={cat.id}
+              >
+                {cat.name}
+              </Checkbox>
+            ))}
           </fieldset>
         </div>
 
@@ -120,61 +127,65 @@ function CatalogView({
       </div>
 
       <ul className="grid w-full grid-cols-2 gap-3 lg:grid-cols-3">
-        {filteredProducts.map((item) => (
-          <li
-            className="min-h-[146px] rounded-xl rounded-tl-none bg-[#fdfdfd] p-1 pb-3 lg:min-h-[428px] lg:rounded-3xl lg:rounded-tl-none"
-            key={item.id}
-          >
-            <Link href={`/catalog/flowers/${item.id}`}>
-              <div>
-                <div className="relative mb-2 h-24 overflow-hidden rounded-tr-xl bg-pink-200 lg:h-60 lg:rounded-tr-3xl">
-                  <small className="absolute top-1">Хит продаж</small>
-                  <small className="absolute top-4">Новинка</small>
-                  <small className="absolute top-8">Букет дня</small>
-                  <small className="absolute bottom-1">Скидка 50%</small>
-                  {/* FIXME: Добавить корректное получение ссылок из PayloadCMS */}
-                  {item.images?.[0] && (
-                    <Image
-                      src={"/api/media/file/buq-6.jpg"}
-                      width={266}
-                      height={245}
-                      alt={item?.description ?? "Цветы."}
-                      className="block h-full w-full object-cover"
-                    />
-                  )}
-                </div>
-                <Text
-                  font={TextFont.MONTSERRAT}
-                  weight={TextWeight.MEDIUM}
-                  size={TextSize.SMALL}
-                  className="mb-2 lg:mb-16"
-                >
-                  {item.name}
-                </Text>
-                <div className="mx-auto flex items-center justify-end px-2">
-                  <p className="g-1 flex flex-col items-center justify-center">
-                    <s className="font-montserrat text-[13px] font-medium">
-                      {Math.ceil(
-                        ((Number.parseInt(item.price) || 0) / 100) * 75,
-                      )}{" "}
-                      р.
-                    </s>
-                    <Text
-                      font={TextFont.MONTSERRAT}
-                      weight={TextWeight.MEDIUM}
-                      size={TextSize.EXTRA_SMALL}
-                      className="text-[#7EA048]"
-                    >
-                      {item.price} р.
-                    </Text>
-                  </p>
+        {filteredProducts.map((item) => {
+          const coverImage = item.images?.at(0) as Media;
+          const imageUrl = coverImage?.url;
 
-                  <Button className="ms-4">Посмотреть</Button>
+          return (
+            <li
+              className="min-h-[146px] rounded-xl rounded-tl-none bg-[#fdfdfd] p-1 pb-3 lg:min-h-[428px] lg:rounded-3xl lg:rounded-tl-none"
+              key={item.id}
+            >
+              <Link href={`/catalog/flowers/${item.id}`}>
+                <div>
+                  <div className="relative mb-2 h-24 overflow-hidden rounded-tr-xl bg-pink-200 lg:h-60 lg:rounded-tr-3xl">
+                    <small className="absolute top-1">Хит продаж</small>
+                    <small className="absolute top-4">Новинка</small>
+                    <small className="absolute top-8">Букет дня</small>
+                    <small className="absolute bottom-1">Скидка 50%</small>
+                    {imageUrl && (
+                      <Image
+                        src={imageUrl}
+                        width={266}
+                        height={245}
+                        alt={item?.description ?? "Цветы."}
+                        className="block h-full w-full object-cover"
+                      />
+                    )}
+                  </div>
+                  <Text
+                    font={TextFont.MONTSERRAT}
+                    weight={TextWeight.MEDIUM}
+                    size={TextSize.SMALL}
+                    className="mb-2 lg:mb-16"
+                  >
+                    {item.name}
+                  </Text>
+                  <div className="mx-auto flex items-center justify-end px-2">
+                    <p className="g-1 flex flex-col items-center justify-center">
+                      <s className="font-montserrat text-[13px] font-medium">
+                        {Math.ceil(
+                          ((Number.parseInt(item.price) || 0) / 100) * 75,
+                        )}{" "}
+                        р.
+                      </s>
+                      <Text
+                        font={TextFont.MONTSERRAT}
+                        weight={TextWeight.MEDIUM}
+                        size={TextSize.EXTRA_SMALL}
+                        className="text-[#7EA048]"
+                      >
+                        {item.price} р.
+                      </Text>
+                    </p>
+
+                    <Button className="ms-4">Посмотреть</Button>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </li>
-        ))}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
