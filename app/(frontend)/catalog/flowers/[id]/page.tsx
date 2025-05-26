26fa73f7c0eab13839834a78855537fc2163c558
Slava Haikin <vyachowski@gmail.com>
2025-05-26 13:36:29 +0300

    fix: add param fix for server component

--- app/(frontend)/catalog/flowers/[id]/page.tsx
+++ app/(frontend)/catalog/flowers/[id]/page.tsx
@@ -1,11 +1,10 @@
 import ProductPage from "@/app/components/ProductPage/ProductPage";
 import { redirect } from "next/navigation";
 
-export default async function FlowersProductPage({
-  params,
-}: {
-  params: { id: string };
+export default async function FlowersProductPage(props: {
+  params: Promise<{ id: string }>;
 }) {
+  const params = await props.params;
   const id = Number.parseInt(params.id);
 
   if (Number.isNaN(id)) {
