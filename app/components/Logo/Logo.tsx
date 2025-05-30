f35d862b00788e0e466370ebed63475926c31ad8
Slava Haikin <vyachowski@gmail.com>
2025-05-30 21:28:36 +0300

    refactor: add nice resolution image

--- app/components/Logo/Logo.tsx
+++ app/components/Logo/Logo.tsx
@@ -1,15 +1,38 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../../../static/logo.png";
+
+import logoImage from "@/static/logo-image.png";
+import logoText from "@/static/logo-text.svg";
+import logoSecondaryText from "@/static/logo-secondary-text.svg";
 
 export default function Logo() {
   return (
-    <Link href={"/"}>
+    <Link
+      href={"/"}
+      className="flex h-[34px] w-[114px] items-center justify-between gap-1 md:h-[54px] md:w-[205px]"
+    >
       <Image
-        src={logo}
-        width={140}
+        src={logoImage}
+        className="h-auto md:w-[46px]"
+        width={28}
         alt="Логотип цветочного магазина Florista."
       />
+      <div className="relative">
+        <Image
+          src={logoText}
+          className="md:h-[32px] md:w-[153px]"
+          width={83}
+          height={17}
+          alt="Логотип цветочного магазина Florista."
+        />
+        <Image
+          src={logoSecondaryText}
+          className="absolute -bottom-2 md:h-[13px] md:w-[150px]"
+          width={82}
+          height={7}
+          alt="Логотип цветочного магазина Florista."
+        />
+      </div>
     </Link>
   );
 }
