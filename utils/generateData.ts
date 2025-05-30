85a66b72e7ae84dd8ff6fa25b94761426396d040
Pavel Alloyarov <pavel.hexlet@mail.ru>
2025-05-31 00:45:58 +0300

    refactor data seeding

--- utils/generateData.ts
+++ utils/generateData.ts
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker";
 import { generateRelation } from "@/utils/generateRelations";
 import data from "@/data";
-import type { ProductCollections } from "@/types";
+import type { GenerateDataOptions, ProductCollections } from "@/types";
 
 const generate = {
   name: (data: string[]) => faker.helpers.arrayElement(data),
@@ -11,12 +11,13 @@ const generate = {
   price: (min = 4000, max = 15000, dec = 0) =>
     faker.commerce.price({ min, max, dec }),
   sale: (probability = 0.4) => faker.datatype.boolean({ probability }),
-  reviewsCount: (min = 0, max = 20) => faker.number.int({ min, max }),
+  reviewsCount: (min = 0, max = 8) => faker.number.int({ min, max }),
 };
 
 const generateCommonFields = <T extends ProductCollections>(
   collection: T,
   imageIds: number[],
+  reviewIds: number[],
 ) => {
   const name = generate.name(data[collection].names);
   const description = generate.description(data[collection].descriptions);
@@ -26,7 +27,7 @@ const generateCommonFields = <T extends ProductCollections>(
   const occasions = generateRelation(data.occasions.ids, 4);
   const tags = generateRelation(data.tags.ids, 4);
   const whom = generateRelation(data.whom.ids, 4);
-  const reviews = generateRelation(data.reviews.ids, generate.reviewsCount());
+  const reviews = generateRelation(reviewIds, generate.reviewsCount());
   const available = true;
 
   return {
@@ -43,42 +44,23 @@ const generateCommonFields = <T extends ProductCollections>(
   } as const;
 };
 
-const generateData = {
-  accessories: (count: number, imageIds: number[]) =>
-    faker.helpers.multiple(
-      () => generateCommonFields("accessories", imageIds),
-      { count },
-    ),
-  baloons: (count: number, imageIds: number[]) =>
-    faker.helpers.multiple(() => generateCommonFields("baloons", imageIds), {
-      count,
-    }),
-  flowers: (count: number, imageIds: number[]) =>
-    faker.helpers.multiple(
-      () => ({
-        ...generateCommonFields("flowers", imageIds),
-        categories: generateRelation(data.categories.ids, 1),
-      }),
-      {
-        count,
-      },
-    ),
-  fruitCarts: (count: number, imageIds: number[]) =>
-    faker.helpers.multiple(() => generateCommonFields("fruitCarts", imageIds), {
-      count,
-    }),
-  indoors: (count: number, imageIds: number[]) =>
-    faker.helpers.multiple(() => generateCommonFields("indoors", imageIds), {
-      count,
-    }),
-  gifts: (count: number, imageIds: number[]) =>
-    faker.helpers.multiple(() => generateCommonFields("gifts", imageIds), {
-      count,
-    }),
-  sweets: (count: number, imageIds: number[]) =>
-    faker.helpers.multiple(() => generateCommonFields("sweets", imageIds), {
-      count,
-    }),
+export const generateData = (
+  collection: ProductCollections,
+  options: GenerateDataOptions,
+) => {
+  return faker.helpers.multiple(
+    () => {
+      return {
+        ...generateCommonFields(
+          collection,
+          options.imageIds,
+          options.reviewIds,
+        ),
+        ...options.extraRelationFields,
+      };
+    },
+    { count: options.count },
+  );
 };
 
 export default generateData;
--- utils/generateRelations.ts
+++ utils/generateRelations.ts
@@ -1,5 +1,9 @@
 import { faker } from "@faker-js/faker";
 
-export function generateRelation(data: number[], count: number): number[] {
+export function generateRelation(data: number[], count: number) {
+  return faker.helpers.uniqueArray(data, count);
+}
+
+export function generateReviewRelation(data: string[], count: number) {
   return faker.helpers.uniqueArray(data, count);
 }
