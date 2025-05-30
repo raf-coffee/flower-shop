a52ee6e8833bb7ad84c483246fad2c1d91610189
Slava Haikin <vyachowski@gmail.com>
2025-05-30 23:20:12 +0300

    refactor: update delivery section

--- app/components/Delivery/Delivery.tsx
+++ app/components/Delivery/Delivery.tsx
@@ -2,27 +2,41 @@ import Image from "next/image";
 
 import deliveryImg from "@/static/delivery/delivery.png";
 
-import { TextFont, TextSize, TextWeight, Text } from "@/app/components/ui";
+import {
+  TextFont,
+  TextSize,
+  TextWeight,
+  Text,
+  Container,
+  Heading,
+} from "@/app/components/ui";
 
 export default function Delivery() {
   return (
-    <section className="bg-main-pink-300">
-      <div className="relative mx-auto h-[360px] max-w-7xl lg:h-[700px]">
-        <h2 className="relative z-10 mb-4 ps-14 pt-10 text-start text-lg font-bold md:text-2xl lg:ps-24 lg:pt-20 lg:text-4xl">
+    <section className="relative bg-main-pink-300">
+      <Image
+        src={deliveryImg}
+        fill
+        className="z-[1]"
+        alt="оформление блока доставка."
+      />
+      <Container className="relative mx-auto h-[360px] lg:h-[700px]">
+        <Heading
+          level={2}
+          className="relative z-10 mb-4 ps-14 pt-10 text-start lg:ps-24 lg:pt-20"
+        >
           Доставка
-        </h2>
-        <p className="absolute left-1/2 top-1/2 z-10 max-w-56 -translate-x-1/2 -translate-y-1/2 text-balance md:max-w-80 md:text-base lg:max-w-xl lg:text-wrap lg:text-lg">
-          <Text
-            font={TextFont.MONTSERRAT}
-            weight={TextWeight.MEDIUM}
-            size={TextSize.LARGE}
-          >
-            Возьмём на себя все заботы по созданию, оформлению и доставке
-            корпоративных букетов в обычные и праздничные дни за разумные деньги
-          </Text>
-        </p>
-        <Image src={deliveryImg} alt="" fill className="z-[1]" />
-      </div>
+        </Heading>
+        <Text
+          font={TextFont.MONTSERRAT}
+          weight={TextWeight.MEDIUM}
+          size={TextSize.LARGE}
+          className="absolute left-1/2 top-1/2 z-10 max-w-56 -translate-x-1/2 -translate-y-1/2 text-balance md:max-w-80 md:text-base lg:max-w-xl lg:text-wrap"
+        >
+          Возьмём на себя все заботы по созданию, оформлению и доставке
+          корпоративных букетов в обычные и праздничные дни за разумные деньги
+        </Text>
+      </Container>
     </section>
   );
 }
