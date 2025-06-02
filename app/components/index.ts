911ac66f0eca55daaa1858885f7206d1df11ef9e
Slava Haikin <vyachowski@gmail.com>
2025-06-02 14:07:30 +0300

    refactor: add success page and route

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,7 @@
 // Pages
-export { default as ErrorPage } from "./pages/ErrorPage/ErrorPage";
 export { default as HomePage } from "./pages/HomePage/HomePage";
+export { default as ErrorPage } from "./pages/ErrorPage/ErrorPage";
+export { default as SuccessPage } from "./pages/SuccessPage/SuccessPage";
 export { default as CatalogPage } from "./pages/CatalogPage/CatalogPage";
 export { default as ContactsPage } from "./pages/ContactsPage/ContactsPage";
 export { default as ProductPage } from "./pages/ProductPage/ProductPage";
---
+++ app/components/pages/SuccessPage/SuccessPage.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/app/components/Hero/Hero";
+import { Button } from "@/app/components/ui";
+
+export default function SuccessPage() {
+  return (
+    <div className="bg-main-pink-300">
+      <Hero
+        heading="Письмо успешно отправлено"
+        description="Мы свяжемся с вами в течение 15 минут."
+      >
+        <a href={"/"} className="hidden w-fit lg:block">
+          <Button size="large">Вернуться на главную</Button>
+        </a>
+      </Hero>
+    </div>
+  );
+}
