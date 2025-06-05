bdf8c54099530789f88ad4a68f8579ef2b58fefb
Pavel Alloyarov <pavel.hexlet@mail.ru>
2025-06-05 10:53:20 +0300

    update images

--- static/decor/index.ts
+++ static/decor/index.ts
@@ -1,5 +1,5 @@
 // Страница "Контакты"
-export { default as Decor1 } from "./decor-1.png";
-export { default as Decor2 } from "./decor-2.png";
-export { default as Decor3 } from "./decor-3.png";
-export { default as Decor4 } from "./decor-4.png";
+export { default as Decor1 } from "./decor-1.webp";
+export { default as Decor2 } from "./decor-2.webp";
+export { default as Decor3 } from "./decor-3.webp";
+export { default as Decor4 } from "./decor-4.webp";
