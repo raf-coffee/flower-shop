96a0277e85ebf4a380bbf8b889de6f643fe185e9
Pavel Alloyarov <pavel.hexlet@mail.ru>
2025-06-07 01:01:36 +0300

    add path alias to vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vitest/config";
-
+import path from "path";
 export default defineConfig({
   test: {
     dir: "./tests/unit",
     globals: true,
+    environment: "node",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "./"),
+    },
   },
 });
