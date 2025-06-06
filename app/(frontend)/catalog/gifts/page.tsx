8718218bb9fe51287df161a6ffe6cc766c5d0028
Pavel Alloyarov <pavel.hexlet@mail.ru>
2025-06-06 19:27:29 +0300

    update pages metadata

--- app/(frontend)/catalog/gifts/page.tsx
+++ app/(frontend)/catalog/gifts/page.tsx
@@ -1,5 +1,12 @@
 import { CatalogPage } from "@/app/components/pages";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Подарки | Floristman - Яркие моменты вашей жизни",
+  description: "Идеальные подарки для любого повода и бюджета.",
+};
+
 export default function GiftsRoute() {
   return <CatalogPage type="gifts" />;
 }
--- app/(frontend)/catalog/sweets/page.tsx
+++ app/(frontend)/catalog/sweets/page.tsx
@@ -1,5 +1,12 @@
 import { CatalogPage } from "@/app/components/pages";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Сладости | Floristman - Яркие моменты вашей жизни",
+  description: "Сладости для любого повода и бюджета.",
+};
+
 export default function SweetsRoute() {
   return <CatalogPage type="sweets" />;
 }
